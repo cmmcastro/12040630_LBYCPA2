39ac2bf37918a865c912835a6de6c65985bf1665
Carlos Miguel Castro <castro20020308@gmail.com>
2023-09-22 19:47:55 +0800

    Update Item1 and Item2

--- Module2/Module2/src/Item1.java
+++ Module2/Module2/src/Item1.java
@@ -3,31 +3,30 @@ import java.util.Stack;
 
 public class Item1 {
 
-    public static void main(String[] args) {
-        int decimal, value, quotient, remainder, counter = 0;
-
-        Stack<Integer> binaryStack = new Stack<>();
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Input a decimal number: "); // User inputs a decimal number
-        decimal = scanner.nextInt();
-        scanner.close();
-        value = decimal;
-
+    public static void convertToBinary(Stack<Integer> binaryStack, int decimalValue){
+        int quotient, remainder;
         do {
-            quotient = value/2;
-            remainder = value % 2;
-            value = quotient;
+            quotient = decimalValue/2;
+            remainder = decimalValue % 2;
+            decimalValue = quotient;
             binaryStack.push(remainder); // Pushes a binary digit (remainder) to the array
-            counter++;
-        } while (value != 0); // Continues to do this loop until the value is 0
+        } while (decimalValue != 0); // Continues to do this loop until the decimal value is 0
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> binaryStack = new Stack<>();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Input a decimal number: "); // User inputs a decimal number
+        int decimalValue = scanner.nextInt();
+        scanner.close();
+
+        convertToBinary(binaryStack, decimalValue); // Program converts decimal value to binary value
 
         System.out.print("Binary Conversion: ");
-
-        while (counter != 0){ // Displays the binary conversion of the decimal number input
+        while (!binaryStack.empty()){ // Displays the binary conversion of the decimal number input
             binaryStack.peek(); // Displays the top of the stack
             System.out.print(binaryStack.pop()); // Removes the digit on the top of the stack
-            counter--;
         }
     }
 }
--- Module2/Module2/src/Item2.java
+++ Module2/Module2/src/Item2.java
@@ -5,6 +5,15 @@ import java.util.Stack;
 
 public class Item2 {
     public static String reverse(String string){
+        /*
+         HOW THE FUNCTION WORKS: (e.g. A + (B * C))
+            1. The function checks the last character of the string first, which will be appended to reversedString
+            2. If the character read is an open parenthesis '(', a close parenthesis ')' will be appended, and vice versa
+            3. If the character read is an operand, it will directly be appended to reverseString
+            4. This process will continue until the last character of the string has been appended
+            5. reversedString will be returned
+         */
+
         char ch;
         String reversedString = "";
         for (int i = 0; i < string.length(); i++){
@@ -28,6 +37,16 @@ public class Item2 {
         return reversedString;
     }
 
+    public static String convertStackToString(Stack<Character> stack){
+        String string = "", reverseString = "";
+        while (!stack.isEmpty()){ // converts stack to string; in reverse though because it's a stack
+            reverseString += stack.peek();
+            stack.pop();
+        }
+        string = reverse(reverseString); // reverses the string so it's in the correct order
+        return string;
+    }
+
     public static String infixToPostfix(String infix, Stack<Character> postfixStack, Stack<Character> stack, List<Character> operator){
         /* 
         HOW THE FUNCTION WORKS: (e.g. Infix: ((A + B * (C/A))) - (B/C)))
@@ -37,8 +56,6 @@ public class Item2 {
             4. Repeat until the infix expression is converted into postfix expression
             5. Return the postfix expression as a string
         */
-
-        String reversePostfix = "", postfix = "";
 
         for (int i = 0; i < infix.length(); i++){ // checks each character in infix
             if (infix.charAt(i) == '(' || operator.contains(infix.charAt(i))){ // checks if infix character is open parenthesis or operator
@@ -60,6 +77,7 @@ public class Item2 {
             }
         }
 
+        // fix for converting infix to prefix
         while (!stack.empty()){
             if (operator.contains(stack.peek())){ // top of stack is operator
                 postfixStack.push(stack.peek());
@@ -74,30 +92,24 @@ public class Item2 {
             }
         }
 
-        while (!postfixStack.isEmpty()){ // converts postfixStack to string; in reverse though because it's a stack
-            reversePostfix += postfixStack.peek();
-            postfixStack.pop();
-        }
-
-        postfix = reverse(reversePostfix); // reverses the string so it's in the correct order
-
+        String postfix = convertStackToString(postfixStack);
         return postfix;
     }
 
     public static String infixToPrefix(String infix, Stack<Character> prefixStack, Stack<Character> stack, List<Character> operator){ // fix code; something wrong not sure yet
         /*
         HOW THE FUNCTION WORKS:
-            1. Reverse the 
+            1. Reverse the infix expression and convert the reversed infix expression to postfix expression
+            2. Reverse the postfix expression, which results to the prefix expression
          */
         
         String prefix = "", reverseInfix = reverse(infix), reversePrefix = infixToPostfix(reverseInfix, prefixStack, stack, operator);
-
         prefix = reverse(reversePrefix);
-
         return prefix;
     }
 
     public static void main(String[] args) {
+        // Infix: ((A+(B*(C/A)))-(B/C))
         Stack<Character> stack = new Stack<>(), postfixStack = new Stack<>(), prefixStack = new Stack<>(); // initialize stacks
         char[] symbols = new char[]{'^','*','/','+','-'}; // initialize operators
         List<Character> operator = new ArrayList<>();
