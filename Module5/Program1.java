6042f36c31d816ed01ae93a253e32d8a41882844
Carlos Miguel Castro <castro20020308@gmail.com>
2023-10-18 16:19:49 +0800

    Module 5

---
+++ Module5/Program1.java
@@ -0,0 +1,68 @@
+package Module5;
+
+class Node {
+    int item;
+    Node left, right;
+
+    public Node(int key) {
+        item = key;
+        left = right = null;
+    }
+}
+
+class Program1 {
+    // Root of Binary Tree
+    Node root;
+
+    Program1() {
+        root = null;
+    }
+
+    void postorder(Node node) {
+        if (node == null)
+            return;
+        // Traverse left
+        postorder(node.left);
+        // Traverse right
+        postorder(node.right);
+        // Traverse root
+        System.out.print(node.item + "->");
+    }
+
+    void inorder(Node node) {
+        if (node == null)
+            return;
+        // Traverse left
+        inorder(node.left);
+        // Traverse root
+        System.out.print(node.item + "->");
+        // Traverse right
+        inorder(node.right);
+    }
+
+    void preorder(Node node) {
+        if (node == null)
+            return;
+        // Traverse root
+        System.out.print(node.item + "->");
+        // Traverse left
+        preorder(node.left);
+        // Traverse right
+        preorder(node.right);
+    }
+
+    public static void main(String[] args) {
+        Program1 tree = new Program1();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+        System.out.println("Inorder traversal");
+        tree.inorder(tree.root);
+        System.out.println("\nPreorder traversal ");
+        tree.preorder(tree.root);
+        System.out.println("\nPostorder traversal");
+        tree.postorder(tree.root);
+    }
+}
